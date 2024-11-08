f0320c976f149e8fcda8ba9e0557c39b4b84b2a8
Felipe Herbst Rosa <felipehrs@gmail.com>
2024-11-08 19:19:32 -0300

    Service A

--- serviceA/internal/weather/usecase/weather_usecase.go
+++ serviceA/internal/weather/usecase/weather_usecase.go
@@ -1,7 +1,14 @@
 package usecase
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
 	apiErrors "github.com/felipehrs/goexpert-labs-otel-serciceA/errors"
+	"github.com/felipehrs/goexpert-labs-otel-serciceA/pkg"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 type WeatherUsecase interface {
@@ -26,5 +33,43 @@ func NewWeatherUsecase() WeatherUsecase {
 }
 
 func (w *weatherUsecase) GetWeatherByCep(zipCode string) (WeatherResponse, error) {
-	return WeatherResponse{}, apiErrors.InvalidZipCode
+
+	if !pkg.IsValidZipCode(zipCode) {
+		return WeatherResponse{}, apiErrors.InvalidZipCode
+	}
+
+	//TODO: Call serviceB to get weather by zipCode
+
+	client := http.Client{
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
+	}
+
+	url := fmt.Sprintf("http://localhost:8080/weather/%s", zipCode)
+
+	resp, err := client.Get(url)
+
+	if err != nil {
+		return WeatherResponse{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		return WeatherResponse{}, apiErrors.NotFoundZipCode
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return WeatherResponse{}, err
+	}
+
+	var response WeatherResponse
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		return WeatherResponse{}, err
+	}
+
+	return response, nil
+
 }
